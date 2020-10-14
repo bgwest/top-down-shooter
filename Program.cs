669a2f3655d3e5690aefab5893ca7573a248b63f
Benjamin West <23172276+bgwest@users.noreply.github.com>
2020-10-13 21:11:43 -0700

    boilerplate monogame for top-down-shooter

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace top_down_shooter
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
